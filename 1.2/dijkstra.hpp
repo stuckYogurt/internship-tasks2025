7ca5880cf54b478533350c9cce9484d11aafa8bf
 Владимир Малахов <vladimirtsupin@gmail.com>
2025-07-08 17:09:33 +0300

    2nd task

---
+++ 1.2/dijkstra.hpp
@@ -0,0 +1,42 @@
+#include <limits>
+#include "graph.hpp"
+
+
+void Dijkstra_path(Graph& graph, Node* root_node) {
+    auto node_count = graph.getNodesCount();
+
+    // if distances are needed elsewhere, return it w/o freeing
+    auto distances = new std::vector<EDGE_WEIGHT_TYPE>(node_count, std::numeric_limits<EDGE_WEIGHT_TYPE>::max());
+    std::vector<bool> visited(node_count, false); // could be changed to bitset if large graphs are at use
+
+    (*distances)[root_node->getId()] = 0;
+
+    for (uns _ = 0; _ < node_count; _++) {
+        long v = -1;
+        for (uns u = 0; u < node_count; u++)
+            if (!visited[u] && (v==-1 || (*distances)[u] < (*distances)[v]))
+                v = u;
+
+        //
+        if (v == -1) {
+            break;
+        }
+
+        visited[v] = true;
+        auto Edges = graph.getNode(v)->getOutEdges();
+        for (auto Edge : Edges)
+            (*distances)[Edge->getDrain()->getId()] = std::min((*distances)[Edge->getDrain()->getId()],
+                                                               (*distances)[v] + Edge->getWeight());
+
+    }
+
+    for (uns i = 0; i < node_count; i++) {
+        if (i == root_node->getId()) continue;
+
+        std::cout << graph.getNode(i)->getMark() << " " <<
+        ((*distances)[i] == std::numeric_limits<EDGE_WEIGHT_TYPE>::max() ? "inf" : std::to_string((*distances)[i])) <<
+        std::endl;
+    }
+
+    delete distances;
+}
