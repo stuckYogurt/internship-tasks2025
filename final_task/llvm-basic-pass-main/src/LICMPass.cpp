725bf59a77161d1ded540cc0b7ec6dbd6dd147cb
 Владимир Малахов <vladimirtsupin@gmail.com>
2025-08-28 19:00:08 +0300

    Issues with LoopNesting

--- final_task/llvm-basic-pass-main/src/LICMPass.cpp
+++ final_task/llvm-basic-pass-main/src/LICMPass.cpp
@@ -43,8 +43,11 @@ class ExtendedPreOrderPass : public AnalysisInfoMixin<ExtendedPreOrderPass> {
     static inline AnalysisKey Key;
 public:
     using Result = ExtendedPreOrder;
+    
 
     Result run(Function& Function, FunctionAnalysisManager& AM) {
+        errs() << "PreOrder DFST on " << Function.getName();
+
         std::vector<uns> last(Function.size(), 0);
         std::vector<BasicBlock*> node(Function.size(), nullptr); // node[id] = elem
         std::map<BasicBlock*, uns> number; // number[elem] = id
@@ -55,13 +58,13 @@ public:
         std::stack<BasicBlock*> workStack;
 
         workStack.push(&Function.getEntryBlock());
+
         while (!workStack.empty()) {
             auto curr = workStack.top();
-
-            number[curr] = current;
-            node[current++] = curr;
-
+            
             if (!discovered[curr->getNumber()]) {
+                number[curr] = current;
+                node[current++] = curr;
                 discovered[curr->getNumber()] = true;
 
                 auto currT = curr->getTerminator();
@@ -78,6 +81,28 @@ public:
             last[number[curr]] = current - 1;
         }
 
+        // errs() << "discovers!\n";
+        // for (auto i : discovered) {
+        //     errs() << i << " ";
+        // }
+
+        // errs() << "lasters\n";
+        // for (auto i : last) {
+        //     errs() << i << " ";
+        // }
+
+        // errs() << "noders\n";
+        // for (auto i : node) {
+        //     errs() << i << " ";
+        // }
+
+        // errs() << "checking preorder num\n";
+        // for (auto& i : number) {
+        //     errs() << i.first << " " << i.second << "\n";
+        // } errs() << "\n\n\n";
+
+
+
         // shrinking return vectors
         uns sumB = 0;
         for (auto iter = discovered.rbegin(); iter != discovered.rend(); ++iter) {
@@ -87,6 +112,7 @@ public:
         }
         last.shrink_to_fit(); node.shrink_to_fit();
 
+        errs() << "exitted DFST\n";
         return {node, last, number};
     }
 };
@@ -126,8 +152,15 @@ public:
     // Algorithm follows Paul Havlak's nesting loops algorithm
     // Original denotement is saved accordingly 
     Result run(Function& Function, FunctionAnalysisManager& AM) {
+        errs() << "LoopNesting on " << Function.getName() << "\n";
+        
+
         auto preOrderRes = AM.getResult<ExtendedPreOrderPass>(Function);
 
+        if (preOrderRes.last.size() == 0) {
+            errs() << "mark1\n";
+        }
+        
         // header[block] = header of the smallest loop block is in
         std::vector<int> header(preOrderRes.last.size(), 0); // default header
         std::vector<BBlock_type> type(preOrderRes.last.size(), BBlock_type::nonheader);
@@ -147,22 +180,26 @@ public:
                 }
             }
 
+            errs() << "uhhh FIND\n";
             return -1; // must not happen
         };
 
         auto UNION = [&unions](uns v, uns w) -> void {
             unions[w].merge(unions[v]);
+            unions[v].clear();
         };
 
         auto isAncestor = [&preOrderRes](uns v, uns w) {
             return (w <= v) && (v <= preOrderRes.last[w]);
         };
         
+        
 
         for (uns w = 0; w < preOrderRes.last.size(); ++w) {
             auto currBB = preOrderRes.node[w];
             for ( auto it = pred_begin(currBB), et = pred_end(currBB); it != et; ++it) {
-                auto v = preOrderRes.number[*it];
+                auto v = preOrderRes.number.at(*it);
+
                 if (isAncestor(w, v)) {
                     backPreds[w].push_back(v);
                 } else {
@@ -171,24 +208,24 @@ public:
             } 
         }
 
-        header[0] = -1;
+        // header[0] = -1;
 
         for (int w = preOrderRes.last.size() - 1; w >= 0; --w) {
-            std::vector<uns> P;
+            std::set<uns> P;
 
             for (auto v : backPreds[w]) {
                 if (v != w) {
-                    P.push_back(FIND(v));
+                    P.emplace(FIND(v));
                 } else {
                     type[w] = BBlock_type::self;
                 }
             }
 
-            std::vector<uns> workList = P;
+            std::vector<uns> workList(P.size());
+            std::copy(P.begin(), P.end(), workList.begin());
             if (!P.empty()) {
                 type[w] = BBlock_type::reducible;
             }
-            
             while (!workList.empty()) {
                 auto x = workList[workList.size() - 1];
                 workList.pop_back();
@@ -198,8 +235,8 @@ public:
                     if (!isAncestor(w, y_)) {
                         type[w] = BBlock_type::irred;
                         nonBackPreds[w].push_back(y_);
-                    } else if (!isVectorMember(P, y_) && y_ != w) {
-                        P.push_back(y_);
+                    } else if (P.find(y_) == P.end() && y_ != w) {
+                        P.emplace(y_);
                         workList.push_back(y_);
                     }
                 }
@@ -211,7 +248,11 @@ public:
             }
 
         }
+        errs() << "exitted LoopNesting\n";
 
+        errs() << "Headers: \n";
+        for (auto a : header) errs() << a << " ";
+        errs() << "\n";
 
         return {preOrderRes, header, type};
     }
@@ -228,21 +269,24 @@ public:
 struct LICMPass : PassInfoMixin<LICMPass> 
 {
     bool isLoopMember(LoopNestingTree& LoopNesting, uns targetLoop, uns BB) {
-        uns headIter = LoopNesting.headers[BB];
+        errs() << "entered isLoopMember \n";
+        int headIter = LoopNesting.headers[BB];
         while (headIter != 0) {
             if (headIter == targetLoop) {
+                errs() << "exitting with true \n";
                 return true;
             }
 
             headIter = LoopNesting.headers[headIter];
         }
-
+        errs() << "exitting with false \n";
         return false;
     }
 
-    BasicBlock* insertBBBetween(std::vector<BasicBlock*> predsBB, BasicBlock* succBB) {
+    BasicBlock* insertBBBetween(std::set<BasicBlock*> predsBB, BasicBlock* succBB) {
+        errs() << "entered insertBBB \n";
         auto newBB = BasicBlock::Create(succBB->getContext(), "", succBB->getParent());
-        newBB->moveAfter(predsBB[0]);
+        newBB->moveAfter(*predsBB.begin());
 
         for (auto predBB : predsBB) {
             auto terminator = predBB->getTerminator();
@@ -251,37 +295,83 @@ struct LICMPass : PassInfoMixin<LICMPass>
 
         IRBuilder<> Builder(newBB);
         Builder.CreateBr(succBB);
-
+        errs() << "exitted insertBBB \n";
         return newBB;
     }
 
     #define insertPreheader insertBBBetween
 
-    void moveToPreheader(LoopNestingTree& LoopNesting, Instruction* targetInst, uns targetLoop) {
-        auto Loop = LoopNesting.PreOrder.node[targetLoop];
+    void moveToPreheader(LoopNestingTree& LoopNesting, Instruction* targetInst, uns targetLoop, std::set<uns>& LoopMembers) {
+        errs() << "entered moveToPreheader \n";
         
-        std::vector<BasicBlock*> preheaderCandidates;
-        for (auto pred : predecessors(Loop)) {
-            uns predPO = LoopNesting.PreOrder.number[pred];
-            if (!isLoopMember(LoopNesting, targetLoop, predPO)) {
-                preheaderCandidates.push_back(pred);
+        // finding entry of the loop
+        auto LoopEntryPointPO = *LoopMembers.begin();
+        // for (auto i : LoopMembers) {
+        //     LoopEntryPointPO = std::min(LoopEntryPointPO, i);
+        // }
+
+        
+        
+        std::set<BasicBlock*> preheaderCandidates;
+        for (auto member : LoopMembers) {
+            for (auto pred : predecessors(LoopNesting.PreOrder.node[member])) {
+                uns predPO = LoopNesting.PreOrder.number[pred];
+                if (!isLoopMember(LoopNesting, targetLoop, predPO)) {
+                    preheaderCandidates.emplace(pred);
+                }
             }
+
+            if (preheaderCandidates.size() > 0) {
+                LoopEntryPointPO = member;
+                break;
+            }
+            errs() << "switching...\n";
         }
+        auto LoopEntryPoint = LoopNesting.PreOrder.node[LoopEntryPointPO];
 
         // if unique non-loop pred, it's preheader
         if (preheaderCandidates.size() == 1) {
             targetInst->removeFromParent();
-            targetInst->insertInto(preheaderCandidates[0], preheaderCandidates[0]->begin());
+            targetInst->insertBefore((*preheaderCandidates.begin())->getTerminator());
+            errs() << "exitted, found 1 \n";
         }
 
         // preheader insertion needed
         else if (preheaderCandidates.size() > 1) {
-            auto preheader = insertPreheader(preheaderCandidates, Loop);
+            auto preheader = insertPreheader(preheaderCandidates, LoopEntryPoint);
             targetInst->removeFromParent();
-            targetInst->insertInto(preheaderCandidates[0], preheaderCandidates[0]->begin());
+            targetInst->insertBefore(preheader->getTerminator());
+            errs() << "exitted, found >1 \n";
         }
 
-        else errs() << "zero non-loop predecessors found!\n";
+        else llvm_unreachable( "zero non-loop predecessors found!\n");
+    }
+
+    bool isSafeToMove(Instruction* I) {
+        if (I->mayReadOrWriteMemory())
+            return false;
+        if (isa<LoadInst>(I))
+            return false;
+        if (isa<StoreInst>(I))
+            return false;
+        
+        // Skip terminators
+        if (I->isTerminator())
+            return false;
+        
+        // Skip PHI nodes
+        if (isa<PHINode>(I))
+            return false;
+        
+        // Skip function calls (unless proven pure)
+        if (isa<CallInst>(I) || isa<InvokeInst>(I))
+            return false;
+        
+        // Skip exception handling instructions
+        if (isa<LandingPadInst>(I) || isa<CatchPadInst>(I) || isa<CleanupPadInst>(I))
+            return false;
+
+        return true;
     }
 
 
@@ -289,28 +379,66 @@ struct LICMPass : PassInfoMixin<LICMPass>
     run(Function &Function, 
 	    FunctionAnalysisManager &AM) 
     {
+        errs() << "\n\nfunction: " << Function.getName() << "\n";
+        bool UpdateFlag = false;
+        
+        // when moving stuff, better keep things updated, might not be optimal
+        Update:
+
         LoopNestingTree LoopNesting = AM.getResult<LoopNestingTreePass>(Function);
 
-        
+        std::map<uns, std::set<uns>> LoopMembers;
+        for (uns i = 0; i < LoopNesting.headers.size(); i++) {
+            if (LoopNesting.types[i] != BBlock_type::nonheader) LoopMembers[i].emplace(i);
+            auto currHead = LoopNesting.headers[i];
+            do {
+                LoopMembers[currHead].emplace(i);
+                currHead = LoopNesting.headers[currHead];
+            } while (currHead != 0);
+        }
+
+        errs() <<"types: \n";
+        for (uns i = 0; i < LoopNesting.types.size(); i++) {
+
+            errs() << LoopNesting.types[i] << " " << *LoopNesting.PreOrder.node[i]->getTerminator() <<'\n';
+        }
+        errs() << "\n";
+        // errs() << LoopNesting.headers.size() << " " << LoopNesting.types.size() << "\n";
 
         // iterate in reverse pre-order
-        for (uns BBlockPOrder = LoopNesting.headers.size() - 1; BBlockPOrder >= 0; --BBlockPOrder) {
+        for (int BBlockPOrder = LoopNesting.headers.size() - 1; BBlockPOrder > 0; --BBlockPOrder) {
+            // errs() << "lol" << BBlockPOrder << " e\n";
             // headers that suit algorithm
             if (LoopNesting.types[BBlockPOrder] == BBlock_type::reducible || 
                 LoopNesting.types[BBlockPOrder] == BBlock_type::self) {
 
                     bool Changed = true;
+                    
                     while (Changed) {
-                        for (uns DescendantPO = BBlockPOrder; DescendantPO <= LoopNesting.PreOrder.last[BBlockPOrder]; ++DescendantPO) {
-                            if (LoopNesting.headers[DescendantPO] == BBlockPOrder) {
+                        Changed = false;
+                        
+                        errs() << "here we go again\n";
+                        
+                        for (auto DescendantPO : LoopMembers[BBlockPOrder]) {
+                            if (UpdateFlag) break;
+
+                            if (isLoopMember(LoopNesting, BBlockPOrder, DescendantPO)) {
                                 for (auto& Def : *(LoopNesting.PreOrder.node[DescendantPO])) {
-                                    if (llvm::isa<llvm::LoadInst>(Def)) {
+                                    if (!isSafeToMove(&Def)) {
                                         continue;
                                     }
 
                                     bool Move = true;
                                     for (uns i = 0; i < Def.getNumOperands(); ++i) {
+                                        errs() << "instruction " << Def << "\n";
                                         auto operand = Def.getOperand(i);
+
+                                        errs() << "operand " << *operand << " at " << *LoopNesting.PreOrder.node[DescendantPO]->getTerminator() << "\n";
+                                        if (llvm::isa<llvm::Constant>(operand)) {
+                                            continue;
+                                        }
+                                        
+
                                         auto operandBB = dyn_cast<Instruction>(operand)->getParent();
                                         uns operandBBPO = LoopNesting.PreOrder.number[operandBB];
 
@@ -320,8 +448,9 @@ struct LICMPass : PassInfoMixin<LICMPass>
                                     }
 
                                     if (!Move) continue;
-                                    moveToPreheader(LoopNesting, &Def, BBlockPOrder);
+                                    moveToPreheader(LoopNesting, &Def, BBlockPOrder, LoopMembers[BBlockPOrder]);
                                     Changed = true;
+                                    goto Update;
                                 }
                             }
                         }
@@ -330,6 +459,8 @@ struct LICMPass : PassInfoMixin<LICMPass>
             
 
         }
+
+        errs() << "\n\n\n" << Function << "\n";
 
 
         return (PreservedAnalyses::none());
--- final_task/llvm-basic-pass-main/tests/input.c
+++ final_task/llvm-basic-pass-main/tests/input.c
@@ -13,12 +13,46 @@ foo(int a) {
 }
 
 int poo() {
+  int a = 6, b = 7;
   for (int i = 0; i < 4; i++) {
+    int j = i;
     do {
-      foo(i);
-    } while (i < 5);
+
+      foo(a*b+a);
+      j++;
+    } while (j < 5);
   }
   return 2;
+}
+
+int test_multiple_invariants(int n) {
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        int a = 5 + 3;
+        int b = 10 * 2;
+        int c = a + b;
+        sum += c;
+    }
+    return sum;
+}
+
+int test_control_flow_invariants(int n) {
+    int result = 0;
+    int invariant_calc = (100 - 50) / 2;  // Should be hoisted: 25
+    
+    for (int i = 0; i < n; i++) {
+        if (i % 2 == 0) {
+            continue;
+        }
+        
+        int temp = invariant_calc * 2;  // Should be hoisted: 25*2=50
+        result += temp;
+        
+        if (result > 1000) {
+            break;
+        }
+    }
+    return result;
 }
 
 
